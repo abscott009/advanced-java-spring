caee83fceb462a975fe8a7e2d1145c8a9f252c06
andrewscott <abscott009@gmail.com>
2023-03-02 11:56:03 -0500

    @ComponentScan LBD

--- src/main/java/platform/codingnomads/co/corespring/examples/componentscanannotation/ComponentScanConfiguration.java
+++ src/main/java/platform/codingnomads/co/corespring/examples/componentscanannotation/ComponentScanConfiguration.java
@@ -3,13 +3,16 @@ package platform.codingnomads.co.corespring.examples.componentscanannotation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import platform.codingnomads.co.corespring.examples.componentscanannotation.componentscanLBD.ComponentOne;
 
 @Configuration
-@ComponentScan(basePackages = "platform.codingnomads.co.corespring.examples.componentscanannotation.tools")
+@ComponentScan(basePackages = "platform.codingnomads.co.corespring.examples.componentscanannotation.componentscanLBD")
 public class ComponentScanConfiguration {
 
     @Bean
     public SampleBean sampleBean() {
         return new SampleBean();
     }
+
+
 }

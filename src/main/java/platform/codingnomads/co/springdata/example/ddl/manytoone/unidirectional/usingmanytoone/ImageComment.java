b595a1bd6179c0a28813e959814f2f6dd3d27ae9
andrewscott <abscott009@gmail.com>
2023-03-13 12:36:16 -0400

    Many to one LBD

---
+++ src/main/java/platform/codingnomads/co/springdata/example/ddl/manytoone/unidirectional/usingmanytoone/ImageComment.java
@@ -0,0 +1,25 @@
+package platform.codingnomads.co.springdata.example.ddl.manytoone.unidirectional.usingmanytoone;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class ImageComment {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(nullable = false, updatable = false)
+        private String username;
+
+        @Column(nullable = false)
+        private String imageId;
+
+        @ManyToOne(cascade = CascadeType.ALL, optional = false)
+        private Comment comment;
+
+}

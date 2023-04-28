ca57f9d55c42fc6b7b65fef278d782a372264cd2
andrewscott <abscott009@gmail.com>
2023-04-28 08:48:48 -0400

    added junit test implementation

--- src/test/java/platform/codingnomads/co/springtest/lab/MovieControllerTest.java
+++ src/test/java/platform/codingnomads/co/springtest/lab/MovieControllerTest.java
@@ -4,13 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import platform.codingnomads.co.springtest.lab.SpringTestLab;
+import platform.codingnomads.co.springtest.lab.repository.MovieRepository;
 import platform.codingnomads.co.springtest.lab.service.MovieService;
 
 import static javax.swing.UIManager.get;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = SpringTestLab.class)
 @AutoConfigureMockMvc
@@ -20,17 +25,11 @@ public class MovieControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    MovieService movieService;
+    MovieRepository movieRepo;
 
-    @Test
+    @org.testng.annotations.Test
     public void testGetAllMoviesSuccess() throws Exception{
-        mockMvc.perform(get("/all"))
 
-                .andExpect(status().isOk())
-
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-
-                .andExpect(jsonPath("$", hasSize(2)));
 
     }
 
@@ -51,6 +50,13 @@ public class MovieControllerTest {
 
     @Test
     public void testGetAllMoviesSuccessMockService() {
+        mockMvc.perform(get("/all"))
+
+                .andExpect(status().isOk())
+
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+
+                .andExpect(jsonPath("$", hasSize(2)));
 
     }
 }

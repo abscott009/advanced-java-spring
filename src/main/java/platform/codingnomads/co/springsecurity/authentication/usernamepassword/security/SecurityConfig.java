4fab36f6d25dbab340e8907ee186ddbfef12d51f
andrewscott <abscott009@gmail.com>
2023-05-10 18:07:09 -0400

    5/10/23

--- src/main/java/platform/codingnomads/co/springsecurity/authentication/usernamepassword/security/SecurityConfig.java
+++ src/main/java/platform/codingnomads/co/springsecurity/authentication/usernamepassword/security/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig {
 //        users.setUsersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?");
 //        return users;
 //    }
-//
+
+//  Useful for prototyping
 //    @Bean
 //    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
 //        Authority userAuth = Authority.builder().authority(RoleEnum.ROLE_USER).build();
